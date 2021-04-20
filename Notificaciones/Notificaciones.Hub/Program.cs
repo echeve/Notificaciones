55adac228e82a2cc8da15276b05dbf33c0c00a41
echeve <echeves@gmail.com>
2021-04-20 16:39:15 +0200

    Estructura inicial del proyecto

---
+++ Notificaciones/Notificaciones.Hub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Notificaciones.Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Notificaciones/TestCliente/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
